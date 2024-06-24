ec9a29e9777607811788c4e126a1be3f9f0a6ad3
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-06-23 21:45:14 -0300

    new: adiciona responsividade na imagem do ContentHeader.tsx

--- src/components/ContentHeader.tsx
+++ src/components/ContentHeader.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const ContentHeader = () => {
   return (
     <div className="flex items-end relative">
-      <img src="../tlou.jpg" alt="series" className="min-w-full h-[95vh] object-fill  mx-auto"/>
+      <img src="../tlou.jpg" alt="series" className="min-w-full w-auto xl:h-[95vh] object-fill  mx-auto"/>
       <div className="absolute flex gap-4 mx-10 my-10">
         <button className="text-center bg-white py-2 px-3 text-lg font-medium rounded-lg text-black">
           Add to list
