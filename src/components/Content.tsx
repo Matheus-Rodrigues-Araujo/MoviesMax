f62090a827232f0929d074a944564fd7d62f5981
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-06-24 16:43:09 -0300

    dummyData.json foi adicionado para testes

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,7 +5,7 @@ const Content = () => {
   return (
     <div className="grid gap-2 bg-gray-950 relative">
       <ContentHeader />
-      <div className="grid gap-3 mb-10 px-10 bg-gray-950 w-full">
+      <div className="grid mb-10 px-10 bg-gray-950 w-full">
         <h2 className="text-white font-medium text-2xl">Series for you</h2>
         <ContentWrapper />
       </div>
